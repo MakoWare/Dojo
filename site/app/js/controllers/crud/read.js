58ec115d22521f52c2d9934a4d8897dad3baf3fc
yintothayang <yintothayang@gmail.com>
2014-10-26 18:39:48 -0700

    added a spinner for async things

--- site/app/js/controllers/crud/read.js
+++ site/app/js/controllers/crud/read.js
@@ -25,14 +25,17 @@ var ReadCtrl = function($scope, $location, GlobalService, ParseService){
     };
 
     $scope.createObject = function(objectType){
+        GlobalService.showSpinner();
         ParseService.createObject(objectType, function(results){
+            GlobalService.dismissSpinner();
             console.log(results);
             var objectTypeLower = objectType.charAt(0).toLowerCase() + objectType.substr(1) + "s";
             var newPath = "/" + objectTypeLower + "/" + results.id;
             console.log(newPath);
-//            $location.path(newPath);
-//            $scope.$apply();
+            $location.path(newPath);
+            $scope.$apply();
         });
+
     },
 
     //Init
