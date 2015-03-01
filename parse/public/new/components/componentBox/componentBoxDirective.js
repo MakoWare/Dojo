7e7600a6e6e8a4c8f86e6d6b7e564ebeb000cd11
yintothayang <yintothayang@gmail.com>
2015-03-01 15:32:39 -0800

    good spot to break

--- parse/public/new/components/componentBox/componentBoxDirective.js
+++ parse/public/new/components/componentBox/componentBoxDirective.js
@@ -7,21 +7,64 @@ var ComponentBoxDirective = BaseDirective.extend({
         this.notifications = notifications;
         this.$scope = $scope;
         this.name = $scope.name;
+        this.setUpComponent();
         this.$timeout = $timeout;
         this._super($scope);
+
+
+
+    },
+
+    setUpComponent: function(){
+        switch(this.name){
+        case "contactList":
+            this.$scope.componentHeaderUrl = "/components/contacts/contactListHeader.html";
+            break;
+        case "dispatchList":
+            this.$scope.componentHeaderUrl = "/components/dispatches/dispatchListHeader.html";
+            break;
+
+        }
+
+
     },
 
     defineListeners: function(){
         var self = this;
+        this.$scope.removeComponent = function(){ self.removeComponent();};
+        this.$scope.minComponent = function(){ self.minComponent();};
+        this.$scope.maxComponent = function(){ self.maxComponent();};
+
         this.$timeout(function(){
-            $("#" + self.name + "Remove").click(function(event){
-                self.removeComponent();
+            $("#" + self.name + "box").resizable({
+                grid: 50,
+                ghost: true,
+                stop: function(event, ui){
+                    //$scope.resize();
+                }
+            });
+
+            $("#" + self.name + "box").draggable({
+                snap: true,
+                containment: "#dashboard"
             });
         });
     },
 
+    //Notify to Remove
     removeComponent: function(){
         this.notifications.notify(components.events.REMOVE_COMPONENT, this.name);
+    },
+
+    //Minimize the Component
+    minComponent: function(){
+        console.log("min");
+    },
+
+    //Maximize the Component
+    maxComponent: function(){
+        console.log("max");
+
     }
 
 
